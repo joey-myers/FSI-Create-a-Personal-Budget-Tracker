8fcfe9557fda5a9b4fd76efb1afa2c46ffbee959
Joey Myers <joeymyers@joeys-imac.lan>
2022-10-19 19:16:21 -0400

    updated

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+//console.log(weeklyExpenses,monthlyExpenses,annualExpenses)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + response
+    console.log(weeklyExpenses)
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + response
+    console.log(monthlyExpenses)
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let response = parseInt(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + response
+    console.log(annualExpenses)
+}
+
